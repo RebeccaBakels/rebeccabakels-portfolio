459f7cc0a759af06692006167b8e87638ae5531e
Rebecca Bakels <rebeccab@city-furniture.com>
2023-03-26 22:39:24 -0400

    refactored background and added story page

---
+++ pages/my-story.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "../styles/MyStory.module.css";
+
+export default function MyStory() {
+  return (
+    <>
+      <div>
+        <h1>My Story</h1>
+        <p>
+          Mid-2019, during the pandemic, I quit my job as a store manager of 5
+          years for a major retail chain after losing all resemblance of
+          work-life balance and noticing my mental health deteriorating. My job
+          was not only unfulfilling but I was made to feel disposable and was
+          subjected to daily verbal abuse. Feeling pushed to the absolute edge I
+          put in my two weeks and made the decision to put all my brainpower and
+          energy into something that would challenge and grow with me.
+        </p>
+        <br />
+        <p>
+          I was inspired by my sister, Michelle Bakels, a talented and driven
+          software developer down here in South Florida who made the switch from
+          Gallery Director and Art-History Major. I thought{" "}
+          <i>well if SHE can do it, so can I</i>. Kidding. In all seriousness, I
+          saw how happy she was and I couldn't remember the last time I could
+          truly say I was happy.
+        </p>
+        <br />
+        <p>
+          So I put in the work, immediately started attending free online tech
+          meetups and seminars through South Florida Tech Hub. I signed up for
+          multiple short courses including: User Experience (UX) Design, Intro
+          to Web Dev, Game Development (this one was just for fun) and after
+          completing these took the plunge into a 10-week Software Engineering
+          bootcamp. Best choice I've ever made. Was it tough? Absolutely, but
+          not for the reason you'd think. It was a mental hurdle to believe I am
+          capable of learning these things, to be comfortable with the unknown
+          and understand how to find the right answers not memorize them.
+          Imposter syndrome hit me hard about 8 weeks in when I realized how
+          much I didn't know and how massive the world of tech is. It felt like
+          I was swimming in a pond only to look around and realize I was
+          actually in the middle of the Pacific Ocean.
+        </p>
+        <br />
+        <p>
+          This is where my support system was key. I had my person, my sister,
+          to talk to in those low moments when I thought I really couldn't make
+          it through. She heard me out, sympathize with my struggle, gave me the
+          reality check I needed, and held me accountable and, before I knew it,
+          I was graduating and the last 10 weeks, 50 days, 400 hours-felt like a
+          fever dream and as I took my certificate, started interviewing,
+          accepted my first job as an intern software engineer, joined the
+          digital software engineering team at City Furniture, then making my
+          first Pull Requests and demoing changes I made to the entire tech
+          department-the dream never faded away.
+        </p>
+        <br />
+        <p>
+          Here I am, a year after I started my bootcamp, still living in that
+          dream Waking up every day excited to start working, making twice as
+          much as I did at the top volume store in my district at my previous
+          job, having my weekends and holidays, able to pick up and visit my
+          friend in North Carolina working remotely, opening up my life to
+          things I thought were impossible.
+        </p>
+        <h3>And you can do it too.</h3>
+        <br />
+      </div>
+    </>
+  );
+}
