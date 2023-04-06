ced62def22b90c0d40548678f480bcadc165e8f0
Rebecca Bakels <rebeccab@city-furniture.com>
2023-04-05 23:06:52 -0400

    updated css and text in story secion

--- pages/my-story.js
+++ pages/my-story.js
@@ -1,68 +1,58 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import styles from "../styles/MyStory.module.css";
 
 export default function MyStory() {
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <h1>My Story</h1>
-        <p>
-          Mid-2019, during the pandemic, I quit my job as a store manager of 5
-          years for a major retail chain after losing all resemblance of
-          work-life balance and noticing my mental health deteriorating. My job
-          was not only unfulfilling but I was made to feel disposable and was
-          subjected to daily verbal abuse. Feeling pushed to the absolute edge I
-          put in my two weeks and made the decision to put all my brainpower and
-          energy into something that would challenge and grow with me.
+        <p className={styles.paragraph}>
+          In mid-2019, amid the pandemic, I left my unfulfilling job as a retail
+          store manager of 5 years due to the lack of work-life balance and the
+          impact it had on my mental health. Feeling pushed to the absolute edge
+          I put in my two weeks and made the decision to utilize all that energy
+          into something that would challenge and grow with me.
         </p>
         <br />
-        <p>
-          I was inspired by my sister, Michelle Bakels, a talented and driven
-          software developer down here in South Florida who made the switch from
-          Gallery Director and Art-History Major. I thought{" "}
-          <i>well if SHE can do it, so can I</i>. Kidding. In all seriousness, I
-          saw how happy she was and I couldn't remember the last time I could
-          truly say I was happy.
+        <p className={styles.paragraph}>
+          My sister, Michelle Bakels, a former Gallery Director and Art-History
+          Major turned software developer, inspired me to make the switch to
+          tech. I saw how happy she was and I couldn't remember the last time I
+          could truly say I was happy.
         </p>
         <br />
-        <p>
-          So I put in the work, immediately started attending free online tech
-          meetups and seminars through South Florida Tech Hub. I signed up for
-          multiple short courses including: User Experience (UX) Design, Intro
-          to Web Dev, Game Development (this one was just for fun) and after
-          completing these took the plunge into a 10-week Software Engineering
-          bootcamp. Best choice I've ever made. Was it tough? Absolutely, but
-          not for the reason you'd think. It was a mental hurdle to believe I am
-          capable of learning these things, to be comfortable with the unknown
-          and understand how to find the right answers not memorize them.
-          Imposter syndrome hit me hard about 8 weeks in when I realized how
-          much I didn't know and how massive the world of tech is. It felt like
-          I was swimming in a pond only to look around and realize I was
-          actually in the middle of the Pacific Ocean.
+        <p className={styles.paragraph}>
+          I began attending free online tech meetups and seminars through South
+          Florida Tech Hub and enrolled in several short courses, including User
+          Experience (UX) Design, Intro to Web Dev, and Game Development. After
+          completing these courses, I took a 10-week Software Engineering
+          bootcamp that was mentally challenging but immensely rewarding. It was
+          a mental hurdle to believe I am capable of learning these things, to
+          be comfortable with the unknown and understand how to find the right
+          answers not memorize them. Imposter syndrome hit me hard about 8 weeks
+          in when I realized how much I didn't know and how massive the world of
+          tech is. It felt like I was swimming in a pond only to look around and
+          realize I was in the middle of the ocean.
         </p>
         <br />
-        <p>
+        <p className={styles.paragraph}>
           This is where my support system was key. I had my person, my sister,
           to talk to in those low moments when I thought I really couldn't make
           it through. She heard me out, sympathize with my struggle, gave me the
-          reality check I needed, and held me accountable and, before I knew it,
-          I was graduating and the last 10 weeks, 50 days, 400 hours-felt like a
-          fever dream and as I took my certificate, started interviewing,
-          accepted my first job as an intern software engineer, joined the
-          digital software engineering team at City Furniture, then making my
-          first Pull Requests and demoing changes I made to the entire tech
-          department-the dream never faded away.
+          reality check I needed, and held me accountable. With her help, I
+          completed the bootcamp and went on to land a job as an intern software
+          engineer at CITY Furniture, where I joined the digital software
+          engineering team. I felt like I was living the dream, making twice as
+          much as I did at my previous job and having my weekends and holidays
+          to enjoy my life.
         </p>
         <br />
-        <p>
-          Here I am, a year after I started my bootcamp, still living in that
-          dream Waking up every day excited to start working, making twice as
-          much as I did at the top volume store in my district at my previous
-          job, having my weekends and holidays, able to pick up and visit my
-          friend in North Carolina working remotely, opening up my life to
-          things I thought were impossible.
+        <p className={styles.paragraph}>
+          Today, years after starting my bootcamp, I still wake up every day
+          excited to work in tech and pursue opportunities that I never thought
+          were possible. <u>If I can do it, so can you.</u>
         </p>
-        <h3>And you can do it too.</h3>
         <br />
       </div>
     </>
